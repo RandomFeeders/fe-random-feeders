eddf5612d37f05648d52fa0d0ccbef7e0d5e3a9a
Gabriel Pupim <gabriel.pda15@gmail.com>
2023-07-25 23:10:22 -0300

    fix: unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { SpinnerService } from './shared/spinner/spinner.service';
 
 describe('App Component', () => {
     beforeEach(() =>
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
-            declarations: [AppComponent],
+            declarations: [AppComponent, SpinnerComponent],
+            providers: [SpinnerService]
         })
     );
 
